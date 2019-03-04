79e2a99682a1bd11c46f89aeb1a1b2a0b9192fd3
candr006 <candr006@ucr.edu>
2019-03-03 22:03:23 -0800

    begin nn calculation

--- CS205Project2/project2.py
+++ CS205Project2/project2.py
@@ -1,1 +1,28 @@
-print 'hello world'
+import math
+
+data= []
+
+def euclidean_distance(a,b):
+	num_feat=len(a)-1
+	i=1
+	total=0
+	while i<num_feat:
+		total+=(math.pow((float(b[i])-float(a[i])),2))
+		i=i+1
+	return math.sqrt(total)
+
+def nearest_neighbor(a):
+	#keeps track of the distance and uses the key of the node to identify it
+	distance_list=[]
+	#find closest neighbor to a in data
+	for d in data:
+		print(euclidean_distance(a,d))
+
+
+
+#open the file for reading and add to the list of lists - 23-34
+f = open("small_input.txt", "r")
+for line in f:
+   data.append(line.split())
+   #print(line)
+nearest_neighbor(data[0])
