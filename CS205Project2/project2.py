5f2a481555851bd84889f7843a009068dcca338f
candr006 <candr006@ucr.edu>
2019-03-05 22:03:04 -0800

    returning nn - unverified

--- CS205Project2/project2.py
+++ CS205Project2/project2.py
@@ -14,10 +14,20 @@ def euclidean_distance(a,b):
 def nearest_neighbor(a):
 	#keeps track of the distance and uses the key of the node to identify it
 	distance_list=[]
-	#find closest neighbor to a in data
-	for d in data:
-		print(euclidean_distance(a,d))
 
+	#find the distance between a and all other nodes
+	for key,d in enumerate(data):
+		#print("key: "+str(key)+" - "+str(d))
+		distance_list.append(euclidean_distance(a,d))
+
+	#sort distance list
+	distance_list.sort()
+	nn_key=-1
+	for key,val in enumerate(distance_list):
+		nn_key=key
+		break
+	print('Nearest neighbor: '+str(data[nn_key]))
+	return data[key]
 
 
 #open the file for reading and add to the list of lists - 23-34
