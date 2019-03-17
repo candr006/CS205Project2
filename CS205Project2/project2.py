675ee87f4a910bbe4af6754c96e4ae38e6a3110f
candr006 <candr006@ucr.edu>
2019-03-16 22:32:53 -0700

    add comments

--- CS205Project2/project2.py
+++ CS205Project2/project2.py
@@ -56,6 +56,8 @@ def getAccuracy(data,feature_list):
 
 	for key,d in enumerate(data):
 		nn_class=nearest_neighbor_class(d,key,feature_list)
+
+		#if the classification of nn is correct then add to correct count
 		if d[0]==nn_class:
 			num_correct=num_correct+1
 
@@ -79,6 +81,8 @@ def leave_one_out_cv(num_features):
 		a=temp_data.pop(i)
 
 		nn_class=nearest_neighbor_class(a,i,features)
+
+		#if the classification of nn is correct then add to correct count
 		if(a[0]==nn_class):
 			num_correct=num_correct+1
 
@@ -91,6 +95,8 @@ def forward_selection(num_features):
 	best_overall_accuracy=float(0)
 
 	print("\nBeginning Search\n")
+
+	#start with 1 feature and add a feature at every level
 	for i in range(1,num_features+1):
 		feature_to_add_at_this_level = 0
 		best_so_far_accuracy=float(0)
@@ -98,17 +104,22 @@ def forward_selection(num_features):
 		for j in range(1,num_features+1):
 
 			if j not in current_set_of_features:
+
+				#temporarily add j to the set of features and test its accuracy
 				temp_features=current_set_of_features[:]
 				temp_features.append(j)
 				accuracy=getAccuracy(data,temp_features)
 				print('     Using features '+str(temp_features)+' accuracy is: '+str(accuracy))
 
+				#if the new feature set is better than the bsf at this level, then add j to current feature set
 				if accuracy>best_so_far_accuracy:
 					best_so_far_accuracy=accuracy
 					feature_to_add_at_this_level=j
 					
 
 		current_set_of_features.append(feature_to_add_at_this_level)
+
+		#keep track of the best overall features and accuracy
 		if(best_so_far_accuracy>best_overall_accuracy):
 			best_overall_accuracy=best_so_far_accuracy
 			best_overall_features=current_set_of_features[:]
@@ -131,6 +142,8 @@ def backward_elimination(num_features):
 	j=num_features
 
 	print("\nBeginning Search\n")
+
+	#start with all features and remove a feature at every level
 	for j in range(1,num_features):
 		feature_to_remove_at_this_level = 0
 		best_so_far_accuracy=float(0)
@@ -139,18 +152,23 @@ def backward_elimination(num_features):
 		for j in range(1,num_features+1):
 
 			if j in current_set_of_features:
+
+				#temporarily remove j from the set of features and test its accuracy
 				temp_features=current_set_of_features[:]
 				ri=temp_features.index(j)
 				temp_features.pop(ri)
 				accuracy=getAccuracy(data,temp_features)
 				print('     Using features '+str(temp_features)+' accuracy is: '+str(accuracy))
 
+				#if the new feature set is better than the bsf at this level, then add j to current feature set
 				if accuracy>best_so_far_accuracy:
 					best_so_far_accuracy=accuracy
 					feature_to_remove_at_this_level=j
 
 		ri=current_set_of_features.index(feature_to_remove_at_this_level)
 		current_set_of_features.pop(ri)
+
+		#keep track of the best overall features and accuracy
 		if(best_so_far_accuracy>=best_overall_accuracy):
 			best_overall_accuracy=best_so_far_accuracy
 			best_overall_features=current_set_of_features[:]
@@ -162,6 +180,8 @@ def backward_elimination(num_features):
 def original_algorithm(num_features):
 	k=1
 	current_set_of_features=[]
+
+	#start with all features in the set
 	while k<=num_features:
 		current_set_of_features.append(k)
 		k=k+1
@@ -172,18 +192,22 @@ def original_algorithm(num_features):
 	i=1
 	while len(current_set_of_features)>0:
 		if (len(current_set_of_features)>1) and (i>1):
+
+			#halve the current set of features
 			first_half=current_set_of_features[:len(current_set_of_features)//2]
 			second_half=current_set_of_features[len(current_set_of_features)//2:]
 
 			if(len(first_half)<len(second_half)):
 				first_half.append(second_half[0])
 
+			#get the accuracy of each half of the features
 			first_accuracy=getAccuracy(data,first_half)
 			print('     Using features '+str(first_half)+' accuracy is: '+str(first_accuracy))
 
 			second_accuracy=getAccuracy(data,second_half)
 			print('     Using features '+str(second_half)+' accuracy is: '+str(second_accuracy))
 			
+			#choose the better of the two halves' accuracies
 			if first_accuracy>second_accuracy:
 				current_set_of_features=first_half[:]
 				best_so_far_accuracy=first_accuracy
@@ -195,12 +219,13 @@ def original_algorithm(num_features):
 			best_so_far_accuracy=getAccuracy(data,current_set_of_features)
 			print('     Using features '+str(current_set_of_features)+' accuracy is: '+str(best_so_far_accuracy))
 
+		#keep track of the best overall accuracy
 		if(best_so_far_accuracy>=best_overall_accuracy):
 			best_overall_accuracy=best_so_far_accuracy
 			best_overall_features=current_set_of_features[:]
 		print("\nFeature set "+str(current_set_of_features)+" was best, accuracy is: "+str(best_so_far_accuracy)+"\n")
 
-
+		#stop when there is only 1 feature in the set
 		if len(current_set_of_features)<2:
 			break
 		i=i+1
@@ -217,12 +242,17 @@ for line in f:
    arr_line=line.split()
    data.append(arr_line)
    num_features=len(arr_line)-1
+
+#take user choice
 ui=raw_input("\n\nType the number of the Algorithm you want to run:\n1.Forward Selection\n2.Backward Elimination\n3.Original Algorithm\n\n")
 message="\n\nThis dataset has "+str(num_features)+" features (not including the class attribute), with "
 message=message+str(len(data))+" instances\n"
 print(message)
+
+#run leave one out cross validation
 leave_one_out_cv(num_features)
 
+#let user pick the algorithm to run
 if ui=='1':
 	forward_selection(num_features)
 if ui=='2':
